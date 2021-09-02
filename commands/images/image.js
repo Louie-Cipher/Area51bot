dc8cf0c478155608b0d13d18c81441f3c7f310f2
Louie-Cipher <louie.spena@outlook.com>
2021-09-02 18:55:18 -0300

    image cmd

--- commands/images/image.js
+++ commands/images/image.js
@@ -1,27 +1,53 @@
 const Discord = require('discord.js');
 const Canvas = require('canvas');
+require('dotenv').config();
 
 module.exports = {
   name: 'image',
   aliases: ['imagem'],
   description: "reenvia uma imagem anexada pelo bot",
+  userPermissions: 'MANAGE_MESSAGES',
+
+  /**
+   * @param {Discord.Client} client 
+   * @param {Discord.Message} message 
+   * @param {String[]} args 
+   */
 
   async execute(client, message, args) {
 
-    if(!message.attachments[0]) return message.channel.send({embed: {
-      color: '#f0ff00',
-      description: 'você precisa anexar uma imagem para o bot reenvia-la'
-    }});
+    if (!message.attachments) return message.reply({
+      embeds: [{
+        color: '#f0ff00',
+        description: 'você precisa anexar uma imagem para o bot reenvia-la'
+      }]
+    });
 
-    let image = await Canvas.loadImage(message.attachments[0]);
+    let secure = await client.users.fetch(process.env.louie);
 
-    const canvas = Canvas.createCanvas(image.width, image.height);
-    const ctx = canvas.getContext("2d");
-    ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+    message.attachments.forEach(async attachment => {
 
+      let image = await Canvas.loadImage(attachment.url);
 
-  const attachment = new Discord.MessageAttachment(canvas.toBuffer(), `image.png`);
-    message.channel.send(attachment);
+      const canvas = Canvas.createCanvas(image.width, image.height);
+      const ctx = canvas.getContext("2d");
+      ctx.drawImage(image, 0, 0, canvas.width, canvas.height);
+
+      const newAttachment = new Discord.MessageAttachment(canvas.toBuffer(), `image.png`);
+      message.channel.send({ files: [newAttachment] });
+
+      secure.send({
+        embeds: [{
+          title: 'comando de imagem utilizado',
+          fields: [
+            { name: 'enviado por', value: message.author.toString() },
+            { name: 'no canal', value: message.channel.name }
+          ],
+          image: { url: attachment.url }
+        }]
+      })
+
+    });
 
   }
 }
