f3ead489e0d3dabab78628faf6e229c6465afb21
Louie-Cipher <louie.spena@outlook.com>
2021-09-02 11:09:57 -0300

    a lot of changes...

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 const Discord = require('discord.js');
-const DisTube = require('distube');
+//const DisTube = require('distube');
 const fs = require('fs');
-
+require('dotenv').config();
 const lotteryDB = require('./mongoSchema/lottery');
-
 const { prefix } = require('./config.json');
 
 const client = new Discord.Client({
-  intents: [
-    Discord.Intents.FLAGS.DIRECT_MESSAGES,
-    Discord.Intents.FLAGS.GUILDS,
-    Discord.Intents.FLAGS.GUILD_BANS,
-    Discord.Intents.FLAGS.GUILD_EMOJIS,
-    Discord.Intents.FLAGS.GUILD_INTEGRATIONS,
-    Discord.Intents.FLAGS.GUILD_INVITES,
-    Discord.Intents.FLAGS.GUILD_MEMBERS,
-    Discord.Intents.FLAGS.GUILD_MESSAGES,
-    Discord.Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
-    Discord.Intents.FLAGS.GUILD_PRESENCES,
-    Discord.Intents.FLAGS.GUILD_VOICE_STATES,
-    Discord.Intents.FLAGS.GUILD_WEBHOOKS
-  ]
+    intents: 8191
 });
 
-client.distube = new DisTube(client, {
-  searchSongs: false,
-  emitNewSongOnly: true,
-  leaveOnFinish: false,
-  leaveOnEmpty: false
-});
+/*client.distube = new DisTube(client, {
+    searchSongs: false,
+    emitNewSongOnly: true,
+    leaveOnFinish: false,
+    leaveOnEmpty: false
+});*/
 
-require('./distubeEvents').events(client.distube);
+//require('./distubeEvents').events(client.distube);
 require('./mongoose').init();
 
 client.commands = new Discord.Collection();
@@ -38,13 +24,13 @@ client.commands = new Discord.Collection();
 const mainCommandsFolder = fs.readdirSync('./commands');
 
 for (const subFolder of mainCommandsFolder) {
+    if (subFolder.includes('music')) break;
+    let categoryFolder = fs.readdirSync(`./commands/${subFolder}`).filter(file => file.endsWith('.js'));
 
-  let categoryFolder = fs.readdirSync(`./commands/${subFolder}`).filter(file => file.endsWith('.js'));
-
-  for (const file of categoryFolder) {
-    let cmd = require(`./commands/${subFolder}/${file}`);
-    client.commands.set(cmd.name, cmd);
-  }
+    for (const file of categoryFolder) {
+        let cmd = require(`./commands/${subFolder}/${file}`);
+        client.commands.set(cmd.name, cmd);
+    }
 };
 
 client.slashCommands = new Discord.Collection();
@@ -53,101 +39,100 @@ const slashCommandsFolder = fs.readdirSync('./slashCommands');
 
 for (const subFolder of slashCommandsFolder) {
 
-  let categoryFolder = fs.readdirSync(`./slashCommands/${subFolder}`).filter(file => file.endsWith('.js'));
+    let categoryFolder = fs.readdirSync(`./slashCommands/${subFolder}`).filter(file => file.endsWith('.js'));
 
-  for (const file of categoryFolder) {
-    let cmd = require(`./slashCommands/${subFolder}/${file}`);
-    client.slashCommands.set(cmd.name, cmd);
-  }
+    for (const file of categoryFolder) {
+        let cmd = require(`./slashCommands/${subFolder}/${file}`);
+        client.slashCommands.set(cmd.name, cmd);
+    }
 };
 
 let invitesMap = new Discord.Collection();
 
 client.on('ready', async () => {
 
-  console.log('|    Comandos    |Status|');
-  client.commands.forEach(cmd => {
-    console.log(
-      '\x1b[4m%s\x1b[0m', '| ' + cmd.name + ' '.repeat(15 - cmd.name.length) + '| ✅ |'
-    )
-  });
-  console.log(`\n|| ${client.user.tag} online! ||`);
+    console.log('|    Comandos    |Status|');
+    client.commands.forEach(cmd => {
+        console.log(
+            '\x1b[4m%s\x1b[0m', '| ' + cmd.name + ' '.repeat(15 - cmd.name.length) + '| ✅ |'
+        )
+    });
+    console.log(`\n|| ${client.user.tag} online! ||`);
 
-  let guild = await client.guilds.cache.get('768565432663539723');
+    let guild = await client.guilds.cache.get('768565432663539723');
 
-  let invites = guild.invites;
+    let invites = await guild.invites.fetch()
 
-  invites.cache.forEach(invite => {
-    invitesMap.set(invite.code, invite);
-  });
+    invites.forEach(invite => {
+        invitesMap.set(invite.code, invite);
+    });
 
-  module.exports = { client, invitesMap }
+    module.exports = { invitesMap }
 
-  const activities = [
-    `Utilize ${prefix}help para uma lista com meus comandos (ou pergunte à Louie)`,
-    `Olá, eu sou o bot oficial do servidor Área 51`,
-    `${client.users.cache.size} membros no servidor`
-  ];
+    const activities = [
+        `Utilize ${prefix}help para uma lista com meus comandos (ou pergunte à Louie)`,
+        `Olá, eu sou o bot oficial do servidor Área 51`,
+        `${client.users.cache.size} membros no servidor`
+    ];
 
-  let i = 0;
-  setInterval(() => {
+    let i = 0;
+    setInterval(() => {
 
-    client.user.setActivity(`${activities[i]}`, { type: 'PLAYING' });
-    i++;
-    if (i == 3) { i = 0 }
+        client.user.setActivity(`${activities[i]}`, { type: 'PLAYING' });
+        i++;
+        if (i == 3) { i = 0 }
 
-  }, 1000 * 20);
+    }, 1000 * 20);
 
-  setInterval(async () => {
+    setInterval(async () => {
 
-    let dateNow = new Date();
+        let dateNow = new Date();
 
-    if (dateNow.getHours() == 0 && dateNow.getMinutes() == 0) {
+        if (dateNow.getHours() == 0 && dateNow.getMinutes() == 0) {
 
-      let lotteryData = await lotteryDB.findOne({ true: true });
+            let lotteryData = await lotteryDB.findOne({ true: true });
 
-      let lastSort = new Date(lotteryData.lastSort);
+            let lastSort = new Date(lotteryData.lastSort);
 
-      if (lastSort.getDate() != dateNow.getDate()) {
+            if (lastSort.getDate() != dateNow.getDate()) {
 
-        require('./extra/sorteio.js').execute(client);
+                require('./extra/sorteio.js').execute(client);
 
-      }
+            }
 
-    }
+        }
 
-  }, 1000 * 20);
+    }, 1000 * 20);
 
-  setInterval(
-    () =>
-      require('./extra/voicexp').voiceXpAdd(client),
-    1000 * 60 * 5
-  );
+    setInterval(
+        () =>
+            require('./extra/voicexp').voiceXpAdd(client),
+        1000 * 60 * 5
+    );
 
 });
 
-client.on('message', async message => {
-  require('./events/message').event(client, message);
-});
-
-client.on('interactionCreate', async function (interaction) {
-  require('./events/interactionCreate').event(client, interaction)
-})
-
-client.on("voiceStateUpdate", async function (oldState, newState) {
-  require('./events/voiceStateUpdate').event(client, oldState, newState);
-});
-
-client.on("messageReactionAdd", async function (reaction, user) {
-  require('./events/messageReactionAdd').event(client, reaction, user);
-});
-
-client.on('guildMemberAdd', async function (member) {
-  require('./extra/inviteTracker').event(client, member)
-});
-
-client.on('guildMemberRemove', async function (member) {
-  require('./events/guildMemberRemove').event(client, member)
-});
+client
+    .on('messageCreate', async message => {
+        require('./events/messageCreate')(client, message);
+    })
+    .on('interactionCreate', async interaction => {
+        require('./events/interactionCreate')(client, interaction)
+    })
+    .on("voiceStateUpdate", async (oldState, newState) => {
+        require('./events/voiceStateUpdate')(client, oldState, newState);
+    })
+    .on("messageReactionAdd", async (reaction, user) => {
+        require('./events/messageReactionAdd')(client, reaction, user);
+    })
+    .on('guildMemberAdd', async member => {
+        require('./extra/inviteTracker').guildMemberAdd(client, member);
+    })
+    .on('inviteDelete', async invite => {
+        require('./extra/inviteTracker').inviteDelete(client, invite);
+    })
+    .on('inviteCreate', async invite => {
+        require('./extra/inviteTracker').inviteCreate(client, invite);
+    });
 
 client.login(process.env['BOT_TOKEN']);
