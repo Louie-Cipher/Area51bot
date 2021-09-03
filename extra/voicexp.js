b8cdd71665e622ca2840a42c29793a2622d590a6
Louie-Cipher <louie.spena@outlook.com>
2021-09-03 18:17:01 -0300

    (tentativa de) code cleanup

--- extra/voicexp.js
+++ extra/voicexp.js
@@ -1,68 +1,61 @@
 const Discord = require('discord.js');
 const profileModel = require('../mongoSchema/profile');
 
-module.exports = {
+/**
+ * @param {Discord.Client} client
+ */
 
-  name: 'Voice XP',
+module.exports = async client => {
 
-  /**
-   * @param {Discord.Client} client
-   */
+  var randomVoiceXP = Math.floor(Math.random() * 2) + 1;
 
-  async voiceXpAdd(client) {
+  let guild = client.guilds.cache.get('768565432663539723');
 
-    console.log('-- Voice XP checking--');
+  for (let channel of guild.channels.cache) {
 
-    var randomVoiceXP = Math.floor(Math.random() * 2) + 1;
+    if (channel[1].type != 'GUILD_VOICE' || channel[1].members.size < 1) continue;
 
-    let guild = client.guilds.cache.get('768565432663539723');
+    let humans = 0
+    channel[1].members.forEach(member => {
+      if (!member.user.bot) { humans++ }
+    })
+    if (humans <= 1) continue;
 
-    for (let channel of guild.channels.cache) {
+    for (let member of channel[1].members) {
 
-      if (channel[1].type == 'GUILD_VOICE' && channel[1].members.size > 1) {
+      if (member[1].user.bot || member[1].voice.mute || member[1].voice.deaf) continue;
 
-        for (let member of channel[1].members) {
+      let profileData = await profileModel.findOne({ userID: member[0] });
 
-          if (!member[1].user.bot && !member[1].voice.mute && !member[1].voice.deaf) {
-
-            console.log('---membro---');
-            console.log(member[1].user.tag);
-
-            let profileData = await profileModel.findOne({ userID: member[0] });
-
-
-            if (!profileData) {
-              let profileNew = await profileModel.create({
-                userID: member[0],
-                chatXP: 1,
-                voiceXP: randomVoiceXP,
-                coins: 100,
-                bank: 200,
-                lastRob: new Date(946684800000),
-                lastWork: new Date(946684800000),
-                lastEditXP: Date.now(),
-                lastEditMoney: Date.now(),
-                lastDaily: Date.now(),
-                created: Date.now()
-              });
-              profileNew.save();
-            }
-            else {
-
-              let profileUpdate = await profileModel.findOneAndUpdate(
-                { userID: member[0] },
-                {
-                  $inc: { voiceXP: randomVoiceXP },
-                  lastEditXP: Date.now()
-                }
-              )
-              profileUpdate.save();
-            }
-
-
-          }
-        }
+      if (!profileData) {
+        let profileNew = await profileModel.create({
+          userID: member[0],
+          chatXP: 1,
+          voiceXP: randomVoiceXP,
+          coins: 100,
+          bank: 200,
+          lastRob: new Date(946684800000),
+          lastWork: new Date(946684800000),
+          lastEditXP: Date.now(),
+          lastEditMoney: Date.now(),
+          lastDaily: Date.now(),
+          created: Date.now()
+        });
+        profileNew.save();
       }
+      else {
+
+        let profileUpdate = await profileModel.findOneAndUpdate(
+          { userID: member[0] },
+          {
+            $inc: { voiceXP: randomVoiceXP },
+            lastEditXP: Date.now()
+          }
+        )
+        profileUpdate.save();
+      }
+
+
     }
 
   }
